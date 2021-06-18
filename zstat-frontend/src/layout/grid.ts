2dd3b5c7a672d07801ed26822094081ba7fd61e5
SkyezerFox <actuallyori@gmail.com>
2021-06-18 11:36:04 +0100

    test: add storybook

--- zstat-frontend/src/layout/grid.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-/**
- * Centers its children in the center of the current grid box.
- */
-export const Centered = styled.div`
-	justify-self: center;
-	align-self: center;
-`;
